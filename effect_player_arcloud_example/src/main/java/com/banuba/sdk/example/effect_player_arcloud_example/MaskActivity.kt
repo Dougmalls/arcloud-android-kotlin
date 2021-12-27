ec19fba672cceb2102327b047142dc240aea1391
Piotr Kulbaka <petr.kulbaka@banuba.com>
2021-12-27 13:34:19 +0300

    fix: crash with 'unload' effect

--- effect_player_arcloud_example/src/main/java/com/banuba/sdk/example/effect_player_arcloud_example/MaskActivity.kt
+++ effect_player_arcloud_example/src/main/java/com/banuba/sdk/example/effect_player_arcloud_example/MaskActivity.kt
@@ -61,7 +61,7 @@ class MaskActivity : AppCompatActivity() {
                 effect = banubaSdkManager.effectManager.loadAsync(maskUri.toString())
             } else {
                 // The mask is unloaded
-                banubaSdkManager.effectManager.unload(effect)
+                banubaSdkManager.effectManager.loadAsync("")
             }
         }
     }
