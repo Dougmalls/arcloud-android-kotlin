c9b93f296e8b7f63274a702bd36a1554cb8fa92a
Dougmalls <102161958+Dougmalls@users.noreply.github.com>
2024-09-27 22:42:03 -0300

    Update BanubaClientToken.kt

--- client_token/com/banuba/sdk/example/common/BanubaClientToken.kt
+++ client_token/com/banuba/sdk/example/common/BanubaClientToken.kt
@@ -4,5 +4,5 @@ package com.banuba.sdk.example.common
  * Client token for Banuba SDK. Consider obfuscation in release app.
  */
 
-internal const val BANUBA_CLIENT_TOKEN: String = <Place your token here>
+internal const val BANUBA_CLIENT_TOKEN: String = <"Qk5CINSM3t5PUyFhr4NYJcOZ3GLVbOY526ApHY2o4Woy84iAABjpVHdWIVLxYNUIu6FDhhL4HGUVGSLHYT48bwVBRUFCkscfbyXDgKqj2S5fJ+UK/ZbbHlbMpTgZfcx2zlL8xvjadjIpuAY/Xp6T3Ne0Sv8syZMNCFijSx2NhtdpzRPLbAisjYxDc78tDSH3ZAt2QGC2ql+lI3573xrsWSP. lxYGfPocawWbyivRfZdwPy4YJJdcuPjPwJAmuuJBtUCnPk8apabm5Nw2kcgqAeJEGjVu4cN8iEE0lQxxVy1WDukXO1Y6QxPU/KBp/tltl9fl/Kvi3zjyz3GnuiqL7ziKkbFDBd/CPHx2sBn+o/+7kbX4PFZJ5T0iu7a07WGeiUgBhY0q1LN0c0J//ZVdL8Gg+fURSOSgevBpG9J+znYstKUqlWYZ0eHIrBGblmQqg3Ij/X4zLVt4K7ZmBHFnzLvM8H8jQIMroCqjWMAxAfcAjCV2VSpOTMdkmCy+ogpI+dXUSFMWJEM2XPnypvlss5Aq6NF7tpeaXwNDIt2eYnXHgkvTEtcfPda8jyAK+fsgs6e1qPqdKUcahmpKM8F+VwFBDAXigbNlP9rVbLho3EHelXisVJUDso+JAflbz4w0w0qF1+yVFp5YaDWDRokBq35bA">
 internal const val BANUBA_AR_CLOUD_URL: String = "https://api.arcloud.banuba.net/v1/effects/far_test_v1.2" // Bucket with Demo effects, can be replaced by your own AR Cloud URL
